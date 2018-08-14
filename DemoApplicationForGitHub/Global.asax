0836457c848fd991318d39b0cb09a41212842996
KiwiTech <KiwiTech@KT-PC-236>
2018-08-14 10:52:20 +0530

    Created Repo and pushed Project on it.

---
+++ DemoApplicationForGitHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoApplicationForGitHub.MvcApplication" Language="C#" %>
---
+++ DemoApplicationForGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoApplicationForGitHub.Startup))]
+namespace DemoApplicationForGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
